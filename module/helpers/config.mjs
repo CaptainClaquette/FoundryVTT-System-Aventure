b7cbbb007f5e91e0f6609312094cd23814fda5d7
DERE KEVIN <kevin.dere@univ-reims.fr>
2023-07-27 09:18:23 +0200

    first commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const AVENTURE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ AVENTURE.abilities = {
+  "str": "AVENTURE.AbilityStr",
+  "dex": "AVENTURE.AbilityDex",
+  "con": "AVENTURE.AbilityCon",
+  "int": "AVENTURE.AbilityInt",
+  "wis": "AVENTURE.AbilityWis",
+  "cha": "AVENTURE.AbilityCha"
+};
+
+AVENTURE.abilityAbbreviations = {
+  "str": "AVENTURE.AbilityStrAbbr",
+  "dex": "AVENTURE.AbilityDexAbbr",
+  "con": "AVENTURE.AbilityConAbbr",
+  "int": "AVENTURE.AbilityIntAbbr",
+  "wis": "AVENTURE.AbilityWisAbbr",
+  "cha": "AVENTURE.AbilityChaAbbr"
+};
